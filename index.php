43e46d63aab747a6bc2a923d84a5f3095b654129
afamusyafa <afamusyafa@gmail.com>
2018-01-07 17:42:32 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Flat desain :Software Development</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/onepage-scroll.css" />
+        <link href='assets/fonts/Righteous.css' rel='stylesheet' type='text/css'>
+        <link href='assets/fonts/Voltaire.css' rel='stylesheet' type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head> 
+    <body>
+        <header>
+            <img src="assets/img/sd.png" width="100px" height="100px" alt="logo mini">
+            <h1><a href="#">Software Development</a></h1>
+        </header>
+        <header>
+         	<nav>
+        		<ul>
+            		<a href="#"><li>Home</li></a>
+               		<a href="#"><li>Download</li></a>
+               	 	<a href="#"><li>Tutorial</li></a>
+                	<a href="#"><li>About Us</li></a>
+            	</ul>
+        	</nav>
+        </header>
+        <div class="main">
+            <section class="page one">
+                <div class="page-container">
+                    <img src="assets/img/wa.png" width="50px" height="50px" alt="logo mini">
+                    <h2>WhatsApp Messenger</h2>
+                    <p align="justify"> WhatsApp Messenger atau WhatsApp saja adalah aplikasi pesan untuk smartphone dengan basic mirip BlackBerry Messenger. WhatsApp Messenger merupakan aplikasi pesan lintas platform yang memungkinkan kita bertukar pesan tanpa biaya SMS, karena WhatsApp Messenger menggunakan paket data internet yang sama untuk email, browsing web, dan lain-lain. Aplikasi WhatsApp Messenger menggunakan koneksi 3G atau WiFi untuk komunikasi data. Dengan menggunakan WhatsApp, kita dapat melakukan obrolan online, berbagi file, bertukar foto dan lain-lain.<br>Source : https://www.youtube.com/watch?v=6HIr_IpSBEQ</p>
+                    <video width="500" controls>
+                    <source src="assets/vid/wa.MP4" align="center" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page two">
+                <div class="page-container">
+                    <img src="assets/img/ig.png" width="50px" height="50px" alt="logo mini">
+                    <h2>Instagram</h2>
+                    <p align="justify">Instagram adalah sebuah aplikasi berbagi foto dan video yang memungkinkan pengguna mengambil foto, mengambil video, menerapkan filter digital, dan membagikannya ke berbagai layanan jejaring sosial, termasuk milik Instagram sendiri. Satu fitur yang unik di Instagram adalah memotong foto menjadi bentuk persegi, sehingga terlihat seperti hasil kamera Kodak Instamatic dan polaroid. Hal ini berbeda dengan rasio aspek 4:3 yang umum digunakan oleh kamera pada peranti bergerak.Instagram dapat digunakan di iPhone, iPad atau iPod Touch versi apapun dengan sistem operasi iOS 7.0 atau yang terbaru,telepon genggam Android apapun dengan sistem operasi versi 2.2 (Froyo) ke atas, dan Windows Phone 8. Aplikasi ini dapat diunggah melalui Apple App Store dan Google Play.Pada tanggal 9 April 2012, diumumkan bahwa Facebook setuju mengambil alih Instagram dengan nilai sekitar $1 miliar..<br>Source : https://www.youtube.com/watch?v=N92MQ9o4Fe0</p>
+                     <video width="500" controls>
+                    <source src="assets/vid/ig.MP4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page three">
+                <div class="page-container">
+                     <img src="assets/img/linux.png" width="70px" height="70px" alt="logo mini">
+                    <h2>Linux Operating System</h2>
+                    <div id="home-artikel-left">
+                        <div class="artikel"><img src="assets/img/redhat.png" width="100" height="100" alt="redhat">
+                        <h1>RedHat</h1>
+                        <b>Nov 01, 2017</b><b>Views : 1,011,497</b>
+                        <a href="index.php">lihat</a>
+                        </div>
+       
+                        <div class="artikel"><img src="assets/img/ubuntu.png" width="100" height="100" alt="ubuntu">
+                        <h1>Ubuntu</h1>
+                        <b>Okt 25, 2017</b><b>Views : 3,487,112</b>
+                        <a href="index.php">lihat</a>
+                        </div>
+                    </div>
+
+                    <div id="home-artikel-right">
+                        <div class="artikel"><img src="assets/img/debian.png" width="100" height="100" alt="debian">
+                        <h1>Debian</h1>
+                        <b>Okt 12, 2017</b><b>Views : 5,532,342</b>
+                        <a href="index.php">lihat</a>
+                        </div>
+                        <div class="artikel"><img src="assets/img/kali.png" width="100" height="100" alt="kali">
+                        <h1>Kali</h1>
+                        <b>Okt 12, 2017</b><b>Views : 5,532,342</b>
+                        <a href="index.php">lihat</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <script type="text/javascript" src="assets/js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.onepage-scroll.js"></script>
+        <script type="text/javascript">
+        $(".main").onepage_scroll({
+                sectionContainer: "section",
+                easting: "cubic-bezier(0.180, 0.900, 0.410, 1.210)"
+        });
+        </script>
+    </body>
+</html>
